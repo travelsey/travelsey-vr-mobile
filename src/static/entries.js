2ef2a622b4d8ec53caa77d6968a6e21a7196786c
tunap <tunappaul@gmail.com>
2020-11-25 17:05:17 +0530

    add GA and prep for beta release

--- src/static/entries.js
+++ src/static/entries.js
@@ -1,21 +1,38 @@
 export const ENTRIES1 = [
-    {
-        title: 'One Day in Dubai',
-        subtitle: 'Dubai is like nowhere else on the planet. From its glittering skyscrapers to its sweeping sand dunes, this multi-cultural metropolis is as awe-worthy as ever. It has the world’s largest shopping mall, the world’s tallest tower, beautiful beaches, and even man-made islands. Experience the best of Dubai in our 360° Virtual Tour!',
-        illustration: 'https://images.pexels.com/photos/1842332/pexels-photo-1842332.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=640&w=426',
-        videourl: 'https://travelsey-vr-api-server.herokuapp.com/api/play?url=https://www.youtube.com/watch?v=mIRge7H4qYs&format=bestvideo'
-
-    },
-    {
-        title: 'One day in Singapore',
-        subtitle: 'The city state of Singapore will charm you with its interesting history, beautiful architecture and friendly attitude. Be amazed by the otherworldly Gardens by the Bay, taste the delicious local food and explore what else this Asian metropolis has to offer. It is also an ideal destination for all shopaholics with its countless shops selling literally everything you can think of. Enjoy the spectacular views during the day and the lit up attractions after nightfall in this 360 video.',
-        illustration: 'https://images.pexels.com/photos/1842332/pexels-photo-1842332.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=640&w=426',
-        videourl: 'https://travelsey-vr-api-server.herokuapp.com/api/play?url=https://www.youtube.com/watch?v=uhdpNod6gb4&format=bestvideo'
-    },
-    {
-        title: 'One day in Venice',
-        subtitle: 'Venice is definitely one-of-a-kind. Where else would you find a city on the water? Its location gives it a unique and a bit mysterious character. And it is also very romantic due to the ever-present bridges and churches. The city centre makes you feel like a time traveller travelling back in time to the époque of Casanova - probably the very best-known citizen of Venice. If you’re lucky enough to visit the city during the carnivals, you will be up for a once-in-a-lifetime experience. And don’t forget to stop by in one of the many restaurants to taste the exquisite local cuisine.',
-        illustration: 'https://i.imgur.com/MABUbpDl.jpg',
-        videourl: 'https://travelsey-vr-api-server.herokuapp.com/api/play?url=https://www.youtube.com/watch?v=cKY-oj0k-Jk&format=bestvideo'
-    }
+  {
+    title: "One Day in Dubai",
+    subtitle:
+      "Dubai is like nowhere else on the planet. From its glittering skyscrapers to its sweeping sand dunes, this multi-cultural metropolis is as awe-worthy as ever. It has the world’s largest shopping mall, the world’s tallest tower, beautiful beaches, and even man-made islands. Experience the best of Dubai in our 360° Virtual Tour!",
+    illustration:
+      "https://images.pexels.com/photos/3769312/pexels-photo-3769312.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=640&w=426",
+    videourl:
+      "https://travelsey-vr-api-server.herokuapp.com/api/play?url=https://www.youtube.com/watch?v=mIRge7H4qYs&format=best",
+  },
+  {
+    title: "One day in Sydney",
+    subtitle:
+      "With its dazzling harbor, world heritage sites and dreamy surf beaches, Sydney is a supermodel of a city. Besides being Australia’s largest city, Sydney is also its most visited. With an incredible variety of attractions and sights to see, it’s easy to see why people come here and stay. Just take a look around in our Best of Sydney 360° Virtual Tour!",
+    illustration:
+      "https://images.pexels.com/photos/2845013/pexels-photo-2845013.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=640&w=426",
+    videourl:
+      "https://travelsey-vr-api-server.herokuapp.com/api/play?url=https://www.youtube.com/watch?v=K6wzEjX006g&format=best",
+  },
+  {
+    title: "One day in Singapore",
+    subtitle:
+      "The city state of Singapore will charm you with its interesting history, beautiful architecture and friendly attitude. Be amazed by the otherworldly Gardens by the Bay, taste the delicious local food and explore what else this Asian metropolis has to offer. It is also an ideal destination for all shopaholics with its countless shops selling literally everything you can think of. Enjoy the spectacular views during the day and the lit up attractions after nightfall in this 360 video.",
+    illustration:
+      "https://images.pexels.com/photos/1842332/pexels-photo-1842332.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=640&w=426",
+    videourl:
+      "https://travelsey-vr-api-server.herokuapp.com/api/play?url=https://www.youtube.com/watch?v=uhdpNod6gb4&format=best",
+  },
+  {
+    title: "One day in Venice",
+    subtitle:
+      "Venice is definitely one-of-a-kind. Where else would you find a city on the water? Its location gives it a unique and a bit mysterious character. And it is also very romantic due to the ever-present bridges and churches. The city centre makes you feel like a time traveller travelling back in time to the époque of Casanova - probably the very best-known citizen of Venice. If you’re lucky enough to visit the city during the carnivals, you will be up for a once-in-a-lifetime experience. And don’t forget to stop by in one of the many restaurants to taste the exquisite local cuisine.",
+    illustration:
+      "https://images.pexels.com/photos/2351439/pexels-photo-2351439.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=640&w=426",
+    videourl:
+      "https://travelsey-vr-api-server.herokuapp.com/api/play?url=https://www.youtube.com/watch?v=cKY-oj0k-Jk&format=best",
+  },
 ];
